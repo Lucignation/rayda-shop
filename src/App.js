df26ae2c6fc002bb05492603a39f580e83221874
Gerald Olumide <lucignation@gmail.com>
2023-03-21 12:08:32 +0100

    mui added to the project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Container maxWidth="sm">
+        Hello
+      </Container>
+    </div>
+  );
+}
+
+export default App;
